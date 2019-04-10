91399ec4987c27c2bd71ffea90732eff7a8a22db
Colby Ford <colbytylerford@gmail.com>
2019-04-09 23:09:15 -0400

    Update Wilcoxon text

--- wilcoxon_test.R
+++ wilcoxon_test.R
@@ -14,8 +14,14 @@ GCSSbyType <- data %>% group_by(GC.SS, Type, .drop = FALSE) %>% tally()
 gc <- GCSSbyType %>% filter(GC.SS == "GC")
 ss <- GCSSbyType %>% filter(GC.SS == "SS")
 
-GCSSbyType_wt <- wilcox.test(gc$n, ss$n)
+GCSSbyType_wt <- wilcox.test(gc$n,
+                             ss$n,
+                             alternative = "two.sided",
+                             paired = TRUE)
 
+GCSSbyType_wt$p.adjust <- p.adjust(GCSSbyType_wt$p.value,
+                                   method = "bonferroni",
+                                   n = nrow(gc))
 
 ######################
 ## GC/SS by Resistance Mechanism
@@ -23,4 +29,26 @@ GCSSbyRM <- data %>% group_by(GC.SS, Resistance.Mechanism, .drop = FALSE) %>% ta
 gc <- GCSSbyRM %>% filter(GC.SS == "GC")
 ss <- GCSSbyRM %>% filter(GC.SS == "SS")
 
-GCSSbyRM_wt <- wilcox.test(gc$n, ss$n)
+GCSSbyRM_wt <- wilcox.test(gc$n,
+                           ss$n,
+                           alternative = "two.sided",
+                           paired = TRUE)
+
+GCSSbyRM_wt$p.adjust <- p.adjust(GCSSbyRM_wt$p.value,
+                                   method = "bonferroni",
+                                   n = nrow(gc))
+
+######################
+## GC/SS by Gene
+GCSSbyGene <- data %>% group_by(GC.SS, AMR.Gene, .drop = FALSE) %>% tally()
+gc <- GCSSbyGene %>% filter(GC.SS == "GC")
+ss <- GCSSbyGene %>% filter(GC.SS == "SS")
+
+GCSSbyGene_wt <- wilcox.test(gc$n,
+                             ss$n,
+                             alternative = "two.sided",
+                             paired = TRUE)
+
+GCSSbyGene_wt$p.adjust <- p.adjust(GCSSbyGene_wt$p.value,
+                                 method = "bonferroni",
+                                 n = nrow(gc))

7a17589976b4873d3fbe7243025daab9bf44f1ac
Colby Ford <colbytylerford@gmail.com>
2019-07-03 09:12:56 -0500

    Update notes in ARM

--- MCR_Analysis/mcr_associationrules.R
+++ MCR_Analysis/mcr_associationrules.R
@@ -14,7 +14,7 @@ library(arules)
 
 ## Generate New Extract from the NCBI Database
 #json.stream <- fromJSON("https://www.ncbi.nlm.nih.gov/pathogens/ngram?start=0&limit=100000&q=%5Bdisplay()%2Chist(geo_loc_name%2Cisolation_source%2Ccollected_by%2Chost%2Cproperty%2Ctarget_creation_date)%5D.from(pathogen).usingschema(%2Fschema%2Fpathogen).matching(status%3D%3D%5B%22current%22%5D+and+q%3D%3D%22taxgroup_name%253A%2522E.coli%2520and%2520Shigella%2522%22).sort(target_creation_date%2Cdesc)&_search=false&rows=20&page=1&sidx=target_creation_date&sord=desc)")
-#saveRDS(json.stream, file = "json.stream.RDS")
+#saveRDS(json.stream, file = "json.stream.RDS") ## to 1/8/2019
 
 ## Read in Data
 json.stream <- readRDS("e.coli.RDS")

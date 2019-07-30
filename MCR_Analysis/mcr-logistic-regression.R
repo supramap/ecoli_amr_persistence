9f24561ee7f87377d325b529b166942e34ccbf93
Gabriel Zenarosa <zenarosa@caa.columbia.edu>
2019-07-30 11:50:22 -0500

    Added code and model using ADASYN

--- MCR_Analysis/mcr-logistic-regression.R
+++ MCR_Analysis/mcr-logistic-regression.R
@@ -8,7 +8,7 @@
 library(jsonlite)
 library(stringr)
 library(glmnet)
-library(caret)
+#library(caret)
 #library(arules)
 
 # Run these *nix commands
@@ -86,94 +86,60 @@ registerDoMC(n_cores)
 
 x <- Matrix(model.matrix(f, gdf.mcr)[, -1], sparse = T)
 y <- Matrix(gdf.mcr$mcr, sparse = T)
+set.seed(123456789)
 model.cv <- cv.glmnet(x = x,
                       y = y,
                       family = "binomial",
-                      #type.measure = "class",
+                      alpha = 1,
+                      #nfolds = floor(length(y) / 10),
                       type.measure = "auc",
-                      keep = TRUE,
-                      parallel = TRUE)
-
+                      parallel = T)
 saveRDS(model.cv, "mcr_lr_model.RDS")
 model.cv <- readRDS("mcr_lr_model.RDS")
 
-coef.mcr <- coef(model.cv, s = "lambda.1se")
+#library(DMwR)
+#gdf.mcr.smote <- SMOTE(mcr ~ .,
+#                       gdf.mcr,
+#                       perc.over = 100 * ceiling(nrow(gdf.mcr[gdf.mcr$mcr == 0,]) / nrow(gdf.mcr[gdf.mcr$mcr == 1,]) - 1),
+#                       perc.under = 100 + (100 * nrow(gdf.mcr[gdf.mcr$mcr == 1,]) / nrow(gdf.mcr[gdf.mcr$mcr == 0,])))
+library(smotefamily)
+gdf.mcr.adasyn <- ADAS(gdf.mcr[,-ncol(gdf.mcr)], gdf.mcr$mcr)
+gdf.mcr.smote <- gdf.mcr.adasyn$data
+colnames(gdf.mcr.smote)[colnames(gdf.mcr.smote) == "class"] <- "mcr"
+#table(gdf.mcr$mcr)
+#table(gdf.mcr.smote$mcr)
+x.smote <- Matrix(model.matrix(f, gdf.mcr.smote)[, -1], sparse = T)
+y.smote <- Matrix(gdf.mcr.smote$mcr, sparse = T)
+set.seed(123456789)
+model.cv.smote <- cv.glmnet(x = x.smote,
+                            y = y.smote,
+                            family = "binomial",
+                            alpha = 1,
+                            nfolds = floor(length(y) / 10),
+                            type.measure = "auc",
+                            parallel = T)
+saveRDS(model.cv.smote, "mcr_lr_model_smote.RDS")
+model.cv.smote <- readRDS("mcr_lr_model_smote.RDS")
+
+### Use adasyn/smote?
+#model.cv <- model.cv.smote
+
+coef.mcr <- coef(model.cv, s = "lambda.min")
 coef.mcr.names <- rownames(coef.mcr)
 coef.mcr.nz <- Matrix(coef.mcr[nonzeroCoef(coef.mcr)], sparse = T)
 rownames(coef.mcr.nz) <- coef.mcr.names[nonzeroCoef(coef.mcr)]
 #write.csv(as.data.frame(exp(cbind(Odds.Ratio=coef.mcr.nz))), "oddsRatioInt.csv")
+View(data.frame(AMR=coef.mcr.names[nonzeroCoef(coef.mcr)], Odds.Ratio=exp(coef.mcr[nonzeroCoef(coef.mcr)])))
 
 ## Look at all prevalidated fits to determine best Lambda, based on Sensitivity
-allfits <- model.cv$fit.preval[,1:length(model.cv$lambda)]
-colnames(allfits) <- paste0("Lambda:",model.cv$lambda)
-allfits$actual <- gdf.mcr$mcr
+# allfits <- as.data.frame(model.cv$fit.preval[,1:length(model.cv$lambda)])
+# colnames(allfits) <- paste0("Lambda:",model.cv$lambda)
+# allfits$actual <- gdf.mcr$mcr
 
 # cor.test(as.numeric(gdf.mcr$mcr), allfits[,1])
 # MLmetrics::Sensitivity(y_pred = allfits[,1],
 #                        y_true = gdf.mcr$mcr,
 #                        positive = lev[1])
-
-############################
-## Using caret to perform CV
-
-## Make cluster
-#library(doParallel)
-#cl <- makeCluster(parallel::detectCores()-1)
-#registerDoParallel(cl)
-
-library(doMC)
-n_cores <- parallel::detectCores()-1
-cat("Number of cores:",n_cores)
-registerDoMC(n_cores)
-
-
-x <- model.matrix(f, gdf.mcr)[, -1]
-xvars <- colnames(x)
-colnames(x) <- make.names(xvars, unique = TRUE, allow_ = FALSE) ## Make syntactically correct var names
-y <- gdf.mcr$mcr
-levels(y) <- c("absent", "present")
-
-library(MLmetrics)
-sensitivity <- function(data, lev = NULL, model = NULL){
-  sens_val <- MLmetrics::Sensitivity(y_pred = data$pred,
-                                     y_true = data$obs,
-                                     positive = lev[1])
-  c(Sensitivity = sens_val)
-}
-
-glmctrl <- trainControl(method = "cv",
-                        number = 5,
-                        returnResamp = "all",
-                        classProbs = TRUE,
-                        summaryFunction = sensitivity,
-                        verboseIter = TRUE)
-set.seed(1337)
-model.cv <- train(x, y,
-                  method = "glmnet", 
-                  trControl = glmctrl,
-                  metric = "Sensitivity",
-                  # metric = "ROC",
-                  tuneGrid = expand.grid(alpha = c(0,1),
-                                         lambda = seq(0.001,
-                                                      0.1,
-                                                      by = 0.01)))
-
-# stopCluster(cl)
-
-saveRDS(model.cv, "mcr_lr_model.RDS")
-
-# model.cv <- readRDS("mcr_lr_model.RDS")
-model.cv <- readRDS("mcr_lr_model_caret.RDS")
-
-model.cv$bestTune
-coef.mcr <- coef(model.cv$finalModel,
-                 model.cv$bestTune$lambda)
-coef.mcr.names <- rownames(coef.mcr)
-coef.mcr.nz <- Matrix(coef.mcr[nonzeroCoef(coef.mcr)], sparse = T)
-rownames(coef.mcr.nz) <- coef.mcr.names[nonzeroCoef(coef.mcr)]
-write.csv(as.data.frame(exp(cbind(Odds.Ratio=coef.mcr.nz))), "oddsRatioInt_cv.csv")
-############################
-
 
 # ------------------
 # Genotype Supersets
@@ -189,34 +155,34 @@ coef.mcr.nz.df <- data.frame(Odds.Ratio=exp(coef.mcr.nz)[-1, 1], Isolates=length
 # New data From Jan 9, 2019 to May 8, 2019
 # json.stream <- fromJSON("https://www.ncbi.nlm.nih.gov/pathogens/ngram?start=0&limit=1000000&q=%5Bdisplay()%2Chist(geo_loc_name%2Cisolation_source%2Cepi_type%2Ccollected_by%2Chost%2Cproperty%2Ctarget_creation_date)%5D.from(pathogen).usingschema(%2Fschema%2Fpathogen).matching(kmer_group%3D%3D%5B%22PDG000000004.1238%22%5D+and+q%3D%3D%22taxgroup_name%253A%2522E.coli%2520and%2520Shigella%2522%22).sort(target_creation_date%2Cdesc)&_search=false&rows=20&page=1&sidx=target_creation_date&sord=desc")
 # saveRDS(json.stream, file = "e.coli_validation.RDS")
-json.stream <- readRDS("e.coli_validation.RDS")
+newjson.stream <- readRDS("e.coli_validation.RDS")
 
-id <- substr(json.stream[["ngout"]][["data"]][["content"]][["id"]], 19, 33)
-creation_date_time <- as.POSIXct(json.stream[["ngout"]][["data"]][["content"]][["target_creation_date"]], format = "%Y-%m-%dT%H:%M:%SZ")
-collection_year <- as.numeric(substr(json.stream[["ngout"]][["data"]][["content"]][["collection_date"]], 1, 4))
-collection_year[is.na(collection_year)] <- as.numeric(format(creation_date_time[is.na(collection_year)], "%Y"))
-location <- json.stream[["ngout"]][["data"]][["content"]][["geo_loc_name"]]
-isolation_type <- json.stream[["ngout"]][["data"]][["content"]][["epi_type"]]
-isolation_source <- json.stream[["ngout"]][["data"]][["content"]][["isolation_source"]]
-df <- data.frame(id, creation_date_time, collection_year, location, isolation_type, isolation_source)
-genotypes <- json.stream[["ngout"]][["data"]][["content"]][["AMR_genotypes"]]
-names(genotypes) <- id
+newid <- substr(newjson.stream[["ngout"]][["data"]][["content"]][["id"]], 19, 33)
+newcreation_date_time <- as.POSIXct(newjson.stream[["ngout"]][["data"]][["content"]][["target_creation_date"]], format = "%Y-%m-%dT%H:%M:%SZ")
+newcollection_year <- as.numeric(substr(newjson.stream[["ngout"]][["data"]][["content"]][["collection_date"]], 1, 4))
+newcollection_year[is.na(newcollection_year)] <- as.numeric(format(newcreation_date_time[is.na(newcollection_year)], "%Y"))
+newlocation <- newjson.stream[["ngout"]][["data"]][["content"]][["geo_loc_name"]]
+newisolation_type <- newjson.stream[["ngout"]][["data"]][["content"]][["epi_type"]]
+newisolation_source <- newjson.stream[["ngout"]][["data"]][["content"]][["isolation_source"]]
+newdf <- data.frame(newid, newcreation_date_time, newcollection_year, newlocation, newisolation_type, newisolation_source)
+newgenotypes <- newjson.stream[["ngout"]][["data"]][["content"]][["AMR_genotypes"]]
+names(newgenotypes) <- newid
 
-genotypes <- genotypes[creation_date_time >= as.POSIXct("2019-01-09 20:14:38", format = "%Y-%m-%d %H:%M:%S")]
-genoid <- id[creation_date_time >= as.POSIXct("2019-01-09 20:14:38", format = "%Y-%m-%d %H:%M:%S")]
+newgenotypes <- newgenotypes[newcreation_date_time >= as.POSIXct("2019-01-09 00:00:00", format = "%Y-%m-%d %H:%M:%S")]
+newgenoid <- newid[newcreation_date_time >= as.POSIXct("2019-01-09 00:00:00", format = "%Y-%m-%d %H:%M:%S")]
 
 
 # ---------
 # Genotypes
 # ---------
 #genotypesall <- unique(sort(unlist(genotypes)))
-genobinv <- lapply(genotypes, function(x){(genotypesall %in% x) * 1})
-gdf <- as.data.frame(matrix(unlist(genobinv), ncol = length(genotypesall), byrow = T))
-names(gdf) <- genotypesall
-gdf.mcr <- gdf[,-grep("mcr", names(gdf))]
-gdf.mcr$mcr <- (rowSums(gdf[,grep("mcr", names(gdf))]) > 0) * 1
-gdf.mcr$mcr <- as.factor(gdf.mcr$mcr)
-# for(col in 1:ncol(gdf.mcr)){gdf.mcr[,col] <- as.factor(gdf.mcr[,col])}
+newgenobinv <- lapply(newgenotypes, function(x){(genotypesall %in% x) * 1})
+newgdf <- as.data.frame(matrix(unlist(newgenobinv), ncol = length(genotypesall), byrow = T))
+names(newgdf) <- genotypesall
+newgdf.mcr <- newgdf[,-grep("mcr", names(newgdf))]
+newgdf.mcr$mcr <- (rowSums(newgdf[,grep("mcr", names(newgdf))]) > 0) * 1
+newgdf.mcr$mcr <- as.factor(newgdf.mcr$mcr)
+# for(col in 1:ncol(newgdf.mcr)){newgdf.mcr[,col] <- as.factor(newgdf.mcr[,col])}
 
 # -------------
 # Genotype Sets
@@ -234,71 +200,12 @@ gdf.mcr$mcr <- as.factor(gdf.mcr$mcr)
 # genotypesets.mcr <- unique(genotypeset.mcr)
 #genotypesets.mcr.gt1 <- genotypesets.mcr[grep(":", genotypesets.mcr)]
 #f <- as.formula(paste("mcr ~ 1+.", paste(genotypesets.mcr.gt1, collapse = "+"), sep = "+"))
-newx <- Matrix(model.matrix(f, gdf.mcr)[, -1], sparse = T)
-newy <- Matrix(gdf.mcr$mcr, sparse = T)
+newx <- Matrix(model.matrix(f, newgdf.mcr)[, -1], sparse = T)
+newy <- Matrix(newgdf.mcr$mcr, sparse = T)
 
 ## Run validation
-#test <- predict(model.cv, newx = x, s = "lambda.1se", type = "response")
-test <- predict(model.cv$finalModel,
-                s = model.cv$lambdaOpt,
-                newx = newx,
-                type = "response")
-trainingprediction <- data.frame(probability = test[,7], actual = as.vector(newy))
-#colnames(trainingprediction) <- c("probability", "actual")
+test.sv <- predict(model.cv, newx = x, s = "lambda.min", type = "response")
+test.cv <- predict(model.cv, newx = newx, s = "lambda.min", type = "response")
 
-cutoff <- seq(0.01, 0.99, by = .01)
-accuracy <- data.frame(cutoff = NA, 
-                       f1 = NA,
-                       sensitivity = NA,
-                       specificity = NA)
-
-for(i in 1:length(cutoff)){
-  
-  c <- cutoff[i]
-  print(c)
-  trainingprediction$prediction <- 0
-  trainingprediction$prediction[trainingprediction$probability >= c] <- 1
-
-  tryCatch({
-    cm <- caret::confusionMatrix(table(trainingprediction$prediction,
-                                       trainingprediction$actual),
-                                 positive = "1")
-    }, warning = print("Skipping", c))
-  
-  if(exists("cm")){
-    f1 <- cm[["byClass"]][["F1"]]
-    sensitivity <- cm[["byClass"]][["Sensitivity"]]
-    specificity <- cm[["byClass"]][["Specificity"]]
-  } else {
-    f1 <- NA
-    sensitivity <- NA
-    specificity <- NA
-  }
-  
-  dt <- data.frame(cutoff = c,
-                   f1 = f1,
-                   sensitivity = sensitivity,
-                   specificity = specificity)
-  accuracy <- rbind(accuracy, dt)
-  rm(cm)
-}
-
-
-# validation <- predict(model.cv, newx = newx , s = "lambda.1se", type = "response")
-validation <- predict(model.cv$finalModel,
-                      s = model.cv$lambdaOpt,
-                      newx = newx,
-                      type = "response")
-validationprediction <- data.frame(probability = test[,7], actual = as.vector(newy))
-validationprediction$prediction <- 0
-validationprediction$prediction[validationprediction$probability >= 0.02] <- 1
-
-caret::confusionMatrix(table(validationprediction$prediction, validationprediction$actual),
-                       positive = "1")
-
-# 
-plot(seq(from = 1, to= 46335, by = 1),
-     sort(trainingprediction$probability),
-     col=trainingprediction$actual)
-
-
+table((test.sv > 0.15) * 1, gdf.mcr$mcr)
+table((test.cv > 0.01) * 1, newgdf.mcr$mcr)
